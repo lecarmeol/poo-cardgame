3f81f90badbb044665216c34acd9bf819a47651a
user <francois.lebun@polygonphysics.com>
2024-02-26 16:51:47 +0100

    Card class defined

--- models/card.py
+++ models/card.py
@@ -1,1 +1,12 @@
+SUITS = ['2', '3','4','5','6',
+         '7','8','9','10','V','D','R','A']
+RANK = ['Pique', 'Coeur', 'Carreau', 'Trefle']
 
+
+class Cards:
+    """Allows all cards to be shuffled and drawn at random"""
+    def __init__(self, suit, rank):
+
+        self.suit = suit
+        self.rank = rank
+        self.is_face_up = False
