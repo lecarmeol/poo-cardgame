12e78e066816849706a30e2d6fa66bd565865354
Alexandre Pellerin <alexandre.pellerin@polygonphysics.com>
2024-02-27 16:33:31 +0100

    Class View finally finished

--- views/View.py
+++ views/View.py
@@ -1,4 +1,5 @@
-REPLY = ["Y", "y", "Yes", "YES", "N", "n", "No", "NO"]
+REPLY_T = ["Y", "y", "Yes", "YES"]
+REPLY_F =["N", "n", "No", "NO"]
 class View:
         """Interface for a Cardgame"""
         def prompt_number_of_player(self, max_nb_player):
@@ -18,16 +19,32 @@ class View:
         def prompt_for_flip(self):
                 """Flip or not"""
                 flip = None
-                while flip is not REPLY :
-                        flip = input("Flip ? [Y/N]")
-                return flip
+                while True:
+                        try:
+                                flip = input("Flip ? [Y/N]")
+                                REPLY_T.index(flip)
+                                return True
+                        except ValueError:
+                                try:
+                                        REPLY_F.index(flip)
+                                        return False
+                                except ValueError:
+                                        pass
 
         def prompt_for_new_game(self):
                 """New game or not"""
                 new_game = None
-                while new_game is not REPLY:
-                        new_game = input("New Game ? [Y/N]")
-                return new_game
+                while True:
+                        try:
+                                new_game = input("New Game ? [Y/N]")
+                                REPLY_T.index(new_game)
+                                return True
+                        except ValueError:
+                                try:
+                                        REPLY_F.index(new_game)
+                                        return False
+                                except ValueError:
+                                        pass
 
         def show_player_hand(self, result):
                 """result = Player hand"""
